61999cb20568eada77feff2437bff135e138604c
Kamil Chojnowski <diath@zodiaclabs.org>
2017-02-13 20:55:58 +0100

    Adds commands command

---
+++ nvsk/plugins/commands.js
@@ -0,0 +1,19 @@
+const Plugin = require('../plugin')
+
+class Commands extends Plugin
+{
+    constructor()
+    {
+        super({
+            name: 'Commands',
+            command: 'commands',
+        })
+    }
+
+    callback(bot, data)
+    {
+        bot.say(data.to, `Available commands (trigger "${bot.trigger}"): ${bot.get_commands().join(', ')}`)
+    }
+}
+
+module.exports.Plugin = Commands
