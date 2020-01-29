3ee1378710c64fe4f1d8864805a06540919114a3
Kamil Chojnowski <contact@diath.net>
2020-01-29 01:52:38 +0100

    Add extra information to the Weather plugin
    * Added wind information (speed, direction).
    * Added pressure information.
    * Updated sunrise and sunset times with leading zeroes.

--- nvsk/plugins/weather.js
+++ nvsk/plugins/weather.js
@@ -1,10 +1,8 @@
 const Plugin = require('../plugin')
 const request = require('request')
 
-class Weather extends Plugin
-{
-    constructor(bot)
-    {
+class Weather extends Plugin {
+    constructor(bot) {
         super({
             name: 'Weather',
             command: ['w', 'weather'],
@@ -12,14 +10,21 @@ class Weather extends Plugin
         })
     }
 
-    ct(timestamp)
-    {
+    ct(timestamp) {
         let date = new Date(timestamp * 1000)
-        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        let hours = date.getHours().toString().padStart(2, '0')
+        let minutes = date.getMinutes().toString().padStart(2, '0')
+        let seconds = date.getSeconds().toString().padStart(2, '0')
+        return `${hours}:${minutes}:${seconds}`
     }
 
-    callback(bot, data)
-    {
+    dir(degrees) {
+        let directions = ['North', 'North-East', 'East', 'South-East', 'South', 'South-West', 'West', 'North-West']
+        let index = Math.round(degrees / 45)
+        return directions[index % directions.length]
+    }
+
+    callback(bot, data) {
         request(`http://api.openweathermap.org/data/2.5/weather?q=${data.param}&units=metric&APPID=${bot.config.tokens.weather}`, (err, response, body) => {
             if (err || response.statusCode != 200) {
                 bot.say(data.to, 'Invalid reply from the server.')
@@ -29,7 +34,7 @@ class Weather extends Plugin
                     bot.say(data.to, info.message)
                 } else {
                     bot.say(data.to,
-                        `${info.name} (${info.sys.country}) (${Math.floor(info.main.temp)}°C) (${info.weather[0].description}, Humidity: ${info.main.humidity}%, Sunrise: ${this.ct(info.sys.sunrise)}, Sunset: ${this.ct(info.sys.sunset)})`
+                        `${info.name} (${info.sys.country}) (${Math.floor(info.main.temp)}°C) (${info.weather[0].description}, Humidity: ${info.main.humidity}%, Pressure: ${info.main.pressure}hPa, Wind: ${info.wind.speed}m/s ${this.dir(info.wind.deg)}, Sunrise: ${this.ct(info.sys.sunrise)}, Sunset: ${this.ct(info.sys.sunset)})`
                     )
                 }
             }
