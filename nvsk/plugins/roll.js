4af48c2e79b2fbf73569c3df9c1348d19f86cd73
Kamil Chojnowski <contact@diath.net>
2020-05-26 00:07:44 +0200

    Catch exceptions from randomjs in the roll plugin

--- nvsk/plugins/roll.js
+++ nvsk/plugins/roll.js
@@ -1,10 +1,8 @@
 const Plugin = require('../plugin')
 const random = require('random-js')
 
-class Roll extends Plugin
-{
-    constructor(bot)
-    {
+class Roll extends Plugin {
+    constructor(bot) {
         super({
             name: 'Roll',
             command: 'roll',
@@ -14,27 +12,27 @@ class Roll extends Plugin
         this.engine = random.engines.mt19937().autoSeed()
     }
 
-    random_number(low, high)
-    {
+    random_number(low, high) {
         return random.integer(low, high)(this.engine)
     }
 
-    random_item(array)
-    {
+    random_item(array) {
         return random.pick(this.engine, array)
     }
 
-    random_dice(count, faces)
-    {
+    random_dice(count, faces) {
         return random.dice(faces, count)(this.engine)
     }
 
-    callback(bot, data)
-    {
+    callback(bot, data) {
         if (data.param.indexOf(',') != -1) {
             let choices = data.param.split(',').map(s => s.trim())
             if (choices.length) {
-                bot.say(data.to, this.random_item(choices))
+                try {
+                    bot.say(data.to, this.random_item(choices))
+                } catch (error) {
+                    bot.say(data.to, error.message)
+                }
             }
         } else if (data.param.indexOf('-') != -1) {
             let args = data.param.split('-')
@@ -43,7 +41,11 @@ class Roll extends Plugin
                 let y = parseInt(args[1], 10)
 
                 if (x !== NaN && y != NaN) {
-                    bot.say(data.to, `${this.random_number(x, y)}`)
+                    try {
+                        bot.say(data.to, `${this.random_number(x, y)}`)
+                    } catch (error) {
+                        bot.say(data.to, error.message)
+                    }
                 }
             }
         } else {
@@ -57,7 +59,11 @@ class Roll extends Plugin
                         bot.say(data.to, 'You can only roll up to 10 dice with up to 36 faces.')
                     } else {
                         let total = this.random_dice(count, faces)
-                        bot.say(data.to, `${total.join(', ')} (${total.reduce((x, y) => x + y)} total)`)
+                        try {
+                            bot.say(data.to, `${total.join(', ')} (${total.reduce((x, y) => x + y)} total)`)
+                        } catch (error) {
+                            bot.say(data.to, error.message)
+                        }
                     }
                 }
             }
